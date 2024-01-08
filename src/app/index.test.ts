4a786ef36ab3686bec5b276b3d64401385c539f1
horisanLog <ttie7374@gmail.com>
2024-01-08 22:28:58 +0900

    browser and mobile

--- src/app/index.test.ts
+++ src/app/index.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, devices } from "@playwright/test"
 
 test("home page should display welcome message", async ({ page }) => {
   // Next.jsアプリケーションのURLに移動
@@ -11,5 +11,28 @@ test("home page should display welcome message", async ({ page }) => {
   // h1タグに特定のテキストが含まれていることを確認
   const title = page.locator("h1")
   await expect(title).toHaveText("Welcome to Next.js with Playwright!")
-  await expect(page).toHaveScreenshot({ fullPage: true })
+  await page.screenshot({ path: `screenshot-example.png` })
+})
+
+test("Cross-browser test", async ({ browser }) => {
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto("http://localhost:3000")
+
+  // h1タグに特定のテキストが含まれていることを確認
+  const title = page.locator("h1")
+  await expect(title).toHaveText("Welcome to Next.js with Playwright!")
+})
+
+test("Mobile emulation", async ({ browser }) => {
+  const iPhone11 = devices["iPhone 11"]
+  const context = await browser.newContext({
+    ...iPhone11,
+  })
+  const page = await context.newPage()
+  await page.goto("http://localhost:3000")
+  
+  // h1タグに特定のテキストが含まれていることを確認
+  const title = page.locator("h1")
+  await expect(title).toHaveText("Welcome to Next.js with Playwright!")
 })
